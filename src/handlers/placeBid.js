4974a109cda7d71c22a022f4c180a14648497c81
Richardbmk <rdobmk@gmail.com>
2022-02-03 21:15:04 +0100

    auth-service done

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,13 +10,26 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
+  // Bid identity validation
+  if (email === auction.seller) {
+    throw new createError.Forbidden(`BID IN YOUR OWN PRODUCT IS FORBIDDEN!!!`);
+  }
+
+  // Avoid double bidding
+  if (email === auction.highestBid.bidder) {
+    throw new createError.Forbidden(`You are already the highest bidder!!!`);
+  }
+
+  // Auction status validation
   if (auction.status !== "OPEN") {
     throw new createError.Forbidden(`You can not bid on closed auctions!`);
   }
 
+  // Amount of Bid validator
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(
       `Your bid must be higher than ${auction.highestBid.amount}!`
@@ -26,9 +39,11 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: "set highestBid.amount = :amount",
+    UpdateExpression:
+      "set highestBid.amount = :amount, highestBid.bidder = :bidder",
     ExpressionAttributeValues: {
       ":amount": amount,
+      ":bidder": email,
     },
     ReturnValues: "ALL_NEW",
   };
